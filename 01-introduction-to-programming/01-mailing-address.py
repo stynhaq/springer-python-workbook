410b3c8260ca441b70357c65a7cfdd10315fd181
Austin (Augustine) Nwaka <stynhaq@live.com>
2022-05-27 20:06:25 +0100

    Add most file

---
+++ 01-introduction-to-programming/01-mailing-address.py
@@ -0,0 +1,9 @@
+"""
+Create a program that displays your name and complete mailing address formatted in
+the manner that you would usually see it on the outside of an envelope. Your program
+does not need to read any input from the user.
+
+"""
+print("Augustine A. Nwaka")
+print("20 Ozumba Mbadiwe Avenue")
+print("Victoria Island, 101241, Lagos.")
